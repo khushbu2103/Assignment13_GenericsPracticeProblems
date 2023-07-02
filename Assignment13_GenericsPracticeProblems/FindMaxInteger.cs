54fda9a6a14863dfa000aaa8c552cafad5b6c860
khushbu <khushbusoni475@gmail.com>
2023-07-02 23:19:09 +0530

    [khushbu] Modify find maximum integer

---
+++ Assignment13_GenericsPracticeProblems/FindMaxInteger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13_GenericsPracticeProblems
+{
+    public class FindMaxInteger
+    {
+        public static int FindMaximumInteger(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is greater value", first);
+                return first;
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is greater value", second);
+                return second;
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater value", third);
+                return third;
+            }
+        }
+
+
+    }
+}
--- Assignment13_GenericsPracticeProblems/Program.cs
+++ Assignment13_GenericsPracticeProblems/Program.cs
@@ -10,6 +10,21 @@ namespace Assignment13_GenericsPracticeProblems
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the generics practice problem");
+
+
+            // Test cases
+            int result1= FindMaxInteger.FindMaximumInteger(10, 30, 60);
+            Console.WriteLine("Test case 1: Max number at first position: " + result1);
+
+            int result2 = FindMaxInteger.FindMaximumInteger(10, 30, 60);
+            Console.WriteLine("Test case 2: Max number at second position: " + result2);
+
+            int result3 = FindMaxInteger.FindMaximumInteger(10, 30, 60);
+            Console.WriteLine("Test case 3: Max number at third position: " + result3);
+            Console.ReadLine();
         }
+
+       
     }
 }
